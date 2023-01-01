890b43e8f5690b271b33f3a577a762810d8776be
Andrew Chen <achen226@gmail.com>
2023-01-01 05:10:09 -0600

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import AppRoutes from './routes'
+import Home from './pages/home';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eat <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <AppRoutes />
+    <div>
+      <h1> example</h1>
+        <Router>
+          <Routes>
+              <Route exact path="/" element={<Home/>} />
+          </Routes>
+        </Router>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>
+                This is the Homie page.
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
